ecbc3f4ed0b7f7f9f97c6b3395174ba2543eb6f5
seb1ast8ian0 <s.staedler05@gmail.com>
2022-11-19 18:14:16 +0100

    Draggable

--- src/Screens/ErrorScreen.js
+++ src/Screens/ErrorScreen.js
@@ -1,7 +1,10 @@
+import Navigation from "../Components/Navigation";
+
 function ErrorScreen() {
 
     return(
         <div className="login-screen">
+        <Navigation/>
             <h1>Error</h1>
         </div>
     );
