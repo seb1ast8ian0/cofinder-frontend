41882cc6b29d702800126214436acb385069895e
Paul Aumann <paul.aumann@gmail.com>
2022-11-19 15:20:27 +0100

    Wildcard

--- src/Components/WildCard.js
+++ src/Components/WildCard.js
@@ -11,7 +11,38 @@ class WildCard extends React.Component {
             <div className="profilepicture">
                 <img src="./cofinder-placeholder.png"/>
             </div>
+            <div className="required-hours">
+              <span className="material-symbols-outlined">schedule</span>
+              <div className="text">20 Stunden benötigt</div>
+            </div>
+
+            <div className="required-experience">
+              <span className="material-symbols-outlined">work_history</span>
+              <div className="text">3 Jahre</div>
+            </div>
             
+            <div className="maximum-salary">
+              <span className="material-symbols-outlined">attach_money</span>
+              <div className="text">90€/h</div>
+            </div>
+
+            <div className="more-info">
+              <div className="title">Benötigte Kenntnisse:</div>
+              <div className="text">UI/UX-Design, Figma, JavaScript, HTML, CSS</div>
+            </div>
+
+            <div className="languages">
+              <div className="flag">
+                <img src="https://countryflagsapi.com/png/de" />
+              </div>
+              <div className="flag">
+                <img src="https://countryflagsapi.com/png/gb" />
+              </div>
+              <div className="flag">
+                <img src="https://countryflagsapi.com/png/ro" />
+              </div>
+            </div>
+
         </div>
       );
     }
