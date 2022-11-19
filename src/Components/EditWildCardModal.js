ec28260e47c7aa828e19852b7043f939b9c3c866
Paul Aumann <paul.aumann@gmail.com>
2022-11-19 18:36:36 +0100

    EditWildCardDescription

--- src/Components/EditWildCardModal.js
+++ src/Components/EditWildCardModal.js
@@ -1,4 +1,5 @@
 import React from "react";
+<<<<<<< HEAD
 
 class EditWildCardModal extends React.Component {
     render() {
@@ -85,6 +86,49 @@ class EditWildCardModal extends React.Component {
                         <button className="confirm">Bestätigen</button>
                     </div>
                 </div>
+=======
+import EditWildCardDescription from "./Forms/EditWildCard/EditWildCardDescription";
+
+class EditWildCardModal extends React.Component {
+    render() {
+        return (
+            <div className="editWildCardModal">
+                <div className="heading">Platzhalter bearbeiten</div>
+                <div className="settings">
+                    <div className="select-requirement">
+                        <div className="title">Anforderung wählen</div>
+                        <ul>
+                            <li className="item">
+                                <span className="material-symbols-outlined">description</span>
+                                <div className="requirement">Bezeichnung</div>
+                            </li>
+                            <li className="item">
+                                <span className="material-symbols-outlined">schedule</span>
+                                <div className="requirement">Stundenzahl</div>
+                            </li>
+                            <li className="item">
+                                <span className="material-symbols-outlined">work_history</span>
+                                <div className="requirement">Arbeitserfahrung</div>
+                            </li>
+                            <li className="item">
+                                <span className="material-symbols-outlined">attach_money</span>
+                                <div className="requirement">Stundenlohn</div>
+                            </li>
+                            <li className="item">
+                                <span className="material-symbols-outlined">code_blocks</span>
+                                <div className="requirement">IT-Kenntnisse</div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="edit-requirement">
+                        <EditWildCardDescription/>
+                    </div>
+                </div>
+                <div className="buttons">
+                    <button className="cancel button-light">Abbrechen</button>
+                    <button className="confirm">Bestätigen</button>
+                </div>
+>>>>>>> f0c867e (EditWildCardDescription)
             </div>
         );
     }
