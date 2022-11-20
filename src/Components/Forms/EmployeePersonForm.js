1687cab53058bc51c2404f43e804cd29528a4936
mqrion <marionamberloenz@gmail.com>
2022-11-20 10:31:11 +0100

    update view

--- src/Components/Forms/EmployeePersonForm.js
+++ src/Components/Forms/EmployeePersonForm.js
@@ -7,23 +7,26 @@ class EmployeePersonForm extends React.Component {
             <div className="employee-person">
                 <form>
                     <div className="data">
-                        <div className="data-group">
+                    <div className="smallgridy">
                             <div className="person-input">
                                 <label htmlFor="surname">Vorname</label>
                                 <input type="text" name="surname" placeholder="Vorname" />
                             </div>
+                            
                             <div className="person-input">
                                 <label htmlFor="lastname">Nachname</label>
                                 <input type="text" name="lastname" placeholder="Nachname"/>
                             </div>
+                            
                         </div>
-                        <div className="data-group">
+                        <div className="smallgridy">
 
                             <div className="small-person-input">
 
                                 <label htmlFor="birthday">Geburtstag</label>
                                 <input type="date" name="birthday" />
                             </div>
+                            
                             <div className="person-input">
 
                                 <label htmlFor="email">Email-adresse</label>
