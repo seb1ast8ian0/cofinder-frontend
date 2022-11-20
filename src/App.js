938d0cc7a84ce173275a889276d9e424bed41803
Paul Aumann <paul.aumann@gmail.com>
2022-11-20 01:59:16 +0100

    Test screen

--- src/App.js
+++ src/App.js
@@ -2,20 +2,12 @@ import {
   BrowserRouter, Route, Routes
 } from "react-router-dom";
 import Header from './Components/Header';
-<<<<<<< HEAD
 import EmployeeBasicData from "./Screens/EmployeeBasicData";
 import ErrorScreen from "./Screens/ErrorScreen";
 import LoginScreen from "./Screens/LoginScreen";
 import ProjectScreen from './Screens/Manager/ProjectScreen';
 import ProjectsScreen from './Screens/Manager/ProjectsScreen';
 import StartScreen from './Screens/StartScreen';
-=======
-import EmployeeMainScreen from './Screens/Employee/EmployeeMainScreen';
-import EmployeeBasicData from "./Screens/EmployeeBasicData";
-import ErrorScreen from "./Screens/ErrorScreen";
-import LoginScreen from "./Screens/LoginScreen";
-import ManagerProjectScreen from './Screens/Manager/ManagerProjectScreen';
->>>>>>> 4c7646421c726d38e67801513b789bb53a1a4eb8
 import TestEditWildCardModalScreen from "./Screens/TestEditWildCardModalScreen";
 import './Styles/main.css';
 
@@ -28,12 +20,8 @@ function App() {
           <Route path="/" element={StartScreen()} />
           <Route path="/login" element={LoginScreen()} />
           <Route path="/employeebasicdata" element={EmployeeBasicData()} />
-<<<<<<< HEAD
           <Route path="/projects" element={ProjectsScreen()} />
           <Route path="/projects/project/:id" element={ProjectScreen()} />
-=======
-          <Route path="/employee" element={EmployeeMainScreen()} />
->>>>>>> 4c7646421c726d38e67801513b789bb53a1a4eb8
           <Route path="/testeditwildcardmodal" element={TestEditWildCardModalScreen()} />
           <Route path="*" element={ErrorScreen()} />
         </Routes>
